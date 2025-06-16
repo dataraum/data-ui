4580314a0570964bcbbf7ee520f81e41b623035c
phas02 <suter.philipp@gmail.com>
2025-06-16 12:12:09 +0200

    basic setup for a new project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	optimizeDeps: {
+		esbuildOptions: {
+			target: 'esnext'
+		}
+	},
+	build: {
+		target: 'esnext'
+	},
 	test: {
 		workspace: [
 			{
