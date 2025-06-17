a8041f497756791fc36496285372106eeb0a1c24
phas02 <suter.philipp@gmail.com>
2025-06-17 17:47:40 +0200

    scrollable markdown component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MdList from '$lib/markdown/md-list.svelte';
 	import TopLeftNav from '$lib/nav/top-left-nav.svelte';
 	import TopRightNav from '$lib/nav/top-right-nav.svelte';
 	import SidePrompt from '$lib/prompt/side-prompt.svelte';
@@ -9,11 +10,21 @@
 		<TopLeftNav />
 		<SidePrompt />
 	</div>
-	<div class="bg-base-100 flex-1">
-		<TopRightNav />
-		<h1>Welcome to SvelteKit</h1>
-		<p>
-			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-		</p>
+	<div class="bg-base-100 overflow-hidden flex-1">
+		<div class="flex flex-col h-full">
+			<!-- Header -->
+			<div class="bg-accent/40 flex flex-row justify-between">
+				<div class="mt-2 flex items-center justify-start pl-4">
+					<h1 class="mb-4 text-2xl font-bold">Welcome to Data Raum</h1>
+				</div>
+				<div>
+					<TopRightNav />
+				</div>
+			</div>
+			<!-- Content Area -->
+			<div class="flex-1 overflow-y-auto p-4 bg-secondary/10">
+				<MdList />
+			</div>
+		</div>
 	</div>
 </div>
