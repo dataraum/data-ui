541748b11f9307e12384507e770351cd0fa763a8
phas02 <suter.philipp@gmail.com>
2025-07-04 10:15:38 +0200

    dependency fix

--- init/user.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_roles.user (id, name, email, email_verified) VALUES (gen_random_uuid (), 'User', 'user@domain.com', NOW())
-INSERT INTO meta_data.workspace (id, workspace_owner) VALUES (gen_random_uuid (), 'user@domain.com')
--- src/routes/signin/+page.server.ts
+++
@@ -1,4 +0,0 @@
-import { signIn } from "$lib/auth"
-import type { Actions } from "./$types"
- 
-export const actions = { default: signIn } satisfies Actions
